d725f3cc2a9de0f7e289a8750acef8b9c7f164d9
67436528220 <67436528220@D69653.trt8.net>
2017-12-19 11:33:46 -0300

    atual

--- hackerrank/src/hackerrank/Aula_1.java
+++ hackerrank/src/hackerrank/Aula_1.java
@@ -13,28 +13,35 @@ public class Aula_1 {
         String s = "HackerRank ";
 		
         Scanner scan = new Scanner(System.in);        
-/* Declare second integer, double, and String variables. */
+        /* Declare second integer, double, and String variables. */
         int num1;
         double num2;
-        String var1;
+        String var1="is the best place to learn and practice coding!";
 
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-        Scanner entrada = new Scanner(System.in);
-        int ent1 = entrada.nextInt();
-        double ent2 = entrada.nextDouble();
-        String ent3 = entrada.nextLine();
+        
+        num1 = scan.nextInt();
+        //scan.nextLine();
+        System.out.println(num1+i);
+        
+        num2 = scan.nextDouble();
+       // scan.nextLine();
+        System.out.println(num2+d);
+        
+        scan.nextLine();
+        var1 = scan.nextLine();
+        System.out.println(s + var1);
         
         /* Print the sum of both integer variables on a new line. */
-        System.out.println(ent1+i);
-System.out.println("tetes");
+        
+        //System.out.println("tetes");
         /* Print the sum of the double variables on a new line. */
-        System.out.println(ent2+d);
+        
 		
         /* Concatenate and print the String variables on a new line; 
         	the 's' variable above should be printed first. */
-
-        System.out.println(s + ent3);
+        
         scan.close();
     }
 }
